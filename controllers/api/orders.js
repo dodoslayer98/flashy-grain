6bdf689e4956707091195f1e7fb8a02824201707
dodoslayer98 <123933669+dodoslayer98@users.noreply.github.com>
2023-04-24 02:12:36 -0400

    added order model

---
+++ controllers/api/orders.js
@@ -0,0 +1,25 @@
+module.exports = {
+    cart,
+    addToCart,
+    setItemQtyInCart,
+    checkout,
+  };
+  
+  // A cart is the unpaid order for a user
+  async function cart(req, res) {
+  
+  }
+  
+  // Add an item to the cart
+  async function addToCart(req, res) {
+  
+  }
+  
+  // Updates an item's qty in the cart
+  async function setItemQtyInCart(req, res) {
+  }
+  
+  // Update the cart's isPaid property to true
+  async function checkout(req, res) {
+  
+  }
